56ce03581b775ed4b9c55be2e652862a2ff0cc4f
claudioasilvadomingues-art <claudioasilvadomingues@gmail.com>
2026-03-16 20:40:34 +0100

    fin

--- session10.js
+++ session10.js
@@ -6,8 +6,18 @@ btn.addEventListener('click', () => {
   fetch('https://api.thecatapi.com/v1/images/search?limit=10')
     .then(response => response.json())
     .then(cats => {
-      console.log('nombre de chats', cats.length, "chats", cats)
-      // oui il y a un bug a corriger, pas sympa..
+        let i = 0;
+        while (i<cats.length) {
+            console.log('nombre de chats', cats.length, 'chats', cats);
+            console.log(cats[i]);
+            let url = cats[i].url;
+            let image = document.createElement('img')
+      
+            image.setAttribute('src', url);
+            gallery.appendChild(image)
+            
+            ++i
+        }
+    
     });
-
-})
+});
