bbd7730bb844a7e72dac397a6d20a06de950b263
claudioasilvadomingues-art <claudioasilvadomingues@gmail.com>
2026-03-16 19:37:20 +0100

    correction1

---
+++ session10.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('#load');
+let gallery = document.querySelector('#gallery');
+
+btn.addEventListener('click', () => {
+
+  fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+    .then(response => response.json())
+    .then(cats => {
+      console.log('nombre de chats', cats.length, "chats", cats)
+      // oui il y a un bug a corriger, pas sympa..
+    });
+
+})
